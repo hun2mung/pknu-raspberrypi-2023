8e605a2af0c335fd9af9e072263cf33e3e0a45e4
hun2mung <ldh5444@naver.com>
2023-06-12 09:23:19 +0900

    add
    add

--- Day03/flask02.py
+++ Day03/flask02.py
@@ -1,11 +1,11 @@
-# flask 웹서버
+# Flask 웹서버
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/') #http://localhost:5000/
+@app.route('/hello')  # http://localhost:5000/hello
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='localhost')
+    app.run(host='localhost', port='8000', debug=True)
